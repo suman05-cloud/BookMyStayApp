e8c834c7a9d75658734ac9b738e04e78a08ff40a
suman patari <patarisuman05@gmail.com>
2026-03-12 10:13:18 +0530

    UC2: Consolidate code into BookMyStayApp.java while keeping sub-classes

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,3 +1,61 @@
+/**
+ * BookMyStayApp - Use Case 2: Basic Room Types & Static Availability
+ */
 public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Hotel Room Initialization\n");
 
+        // Static availability variables
+        int singleRoomAvailability = 5;
+        int doubleRoomAvailability = 3;
+        int suiteRoomAvailability = 2;
+
+        // Room objects
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        System.out.println("Single Room:");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available: " + singleRoomAvailability + "\n");
+
+        System.out.println("Double Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleRoomAvailability + "\n");
+
+        System.out.println("Suite Room:");
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available: " + suiteRoomAvailability);
+    }
+}
+
+// These classes are duplicated but kept as per user request to have them in main file too
+abstract class Room {
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() { super(1, 250, 1500.0); }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() { super(2, 400, 2500.0); }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() { super(3, 750, 5000.0); }
 }
